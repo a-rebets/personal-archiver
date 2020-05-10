4faa9e71c8388984ac99ac8b720af1f5cba89e54
Artem Popelyshev <apopelyshev@gmail.com>
2020-05-10 13:50:12 +0200

    Hot fix: Testing auth module for Probot

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ module.exports = app => {
   app.log('Personal Archiver is running successfully.')
   initBot(app.route('/tg'))
   app.route().get('/setup', (req, res) => {
-    app.log(req.body)
     res.sendStatus(200)
   })
 
@@ -63,9 +62,8 @@ module.exports = app => {
       : { id: null, waitlist: [], track: [], omit: [] }
 
     app.log(`App is running as per [${paramObj.evt.e}]`)
-    if (paramObj.evt.e === 'installation' && paramObj.evt.a === 'created') {
-      await initBase(context)
-    }
+    app.log(app.app)
+    app.log(app.Octokit)
     /* if (paramObj.evt.e === 'installation' && paramObj.evt.a === 'created') {
       bot.onText(/\B\/start\b/, async (msg) => {
         if (!cacheObj.id) {
