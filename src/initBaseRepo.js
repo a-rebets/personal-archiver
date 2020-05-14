5b663cae565054ad7202ed80acc8f6c34e5c7460
Artem Popelyshev <apopelyshev@gmail.com>
2020-05-14 23:05:42 +0200

    (Beta 1.0.5) Template addition process started, new media [no build]

--- src/initBaseRepo.js
+++ src/initBaseRepo.js
@@ -1,5 +1,5 @@
-module.exports = (api) => {
-  context.log('Initiating the base...')
+module.exports = (api, app) => {
+  app.log('Initiating the base...')
   return api.repos.createForAuthenticatedUser({
     name: process.env.BASE_REPO,
     description: 'A repository used by Personal Archiver bot',
